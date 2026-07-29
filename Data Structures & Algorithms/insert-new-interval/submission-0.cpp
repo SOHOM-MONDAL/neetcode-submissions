bdcaf7543850afd3619bdadee6dcb42700667a0e
SOHOM-MONDAL <sohommondal005@gmail.com>
2026-07-29 18:21:44 +0530

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> nums = intervals;
+        nums.push_back(newInterval);
+        sort(nums.begin() , nums.end());
+        vector<vector<int >> ans ;
+        for(int i = 0 ; i < nums.size(); i++){
+            if(ans.empty()|| ans.back()[1] < nums[i][0]){
+                ans.push_back(nums[i]);
+            }
+            else{
+                ans.back()[1] = max(ans.back()[1] , nums[i][1]);
+            }
+        }
+        return ans ;
+    }
+};
